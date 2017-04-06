8750d8e84c32668483b5bb465f02e5680b4f8418
Mols1993 <mols1993@gmail.com>
2017-04-05 22:48:40 -0300

    Initial commit

---
+++ app/src/main/java/com/instants/mols1993/planechase/MainActivity.java
@@ -0,0 +1,29 @@
+package com.instants.mols1993.planechase;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void Planechase(View view){
+        Intent intent = new Intent(this, Planechase.class);
+        intent.putExtra("folder", "planes");
+        intent.putExtra("orientation", "landscape");
+        startActivity(intent);
+    }
+
+    public void Archenemy(View view){
+        Intent intent = new Intent(this, Planechase.class);
+        intent.putExtra("folder", "schemes");
+        intent.putExtra("orientation", "portrait");
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
